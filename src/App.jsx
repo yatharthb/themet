0ee71500f3a1092d79cdb807969ee10373a332b3
yatharthb <51863781+yatharthb@users.noreply.github.com>
2023-05-13 23:38:31 +0000

    Added HSL colors instead of RGB

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [color, setColor] = useState(0);
+  const [hue, setHue] = useState(0);
 
-  // Create a color using the slider value (0-255)
-  const backgroundColor = `rgb(${color}, ${color}, ${color})`;
+  // Create a color using the slider value (0-360)
+  const backgroundColor = `hsl(${hue}, 100%, 50%)`;
+
+  // Determine text color based on hue for contrast
+  const textColor = hue > 200 && hue < 300 ? 'white' : 'black';
 
   return (
-    <div style={{ backgroundColor, minHeight: '100vh', padding: '20px' }}>
+    <div style={{ backgroundColor, color: textColor, minHeight: '100vh', padding: '20px' }}>
       <input
         type="range"
         min="0"
-        max="255"
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
+        max="360"
+        value={hue}
+        onChange={(e) => setHue(e.target.value)}
         style={{ width: '100%', marginBottom: '20px' }}
       />
 
