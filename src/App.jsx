4da28f034e6d147ce8d8a7fa895e5dbb879e7ec2
yatharthb <51863781+yatharthb@users.noreply.github.com>
2023-05-14 11:01:26 +0000

    added radial backgrounds and color changing text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -17,57 +16,50 @@ import {
 import './App.css';
 
 const Navbar = ({ color }) => (
-  <nav className="nav" style={{ backgroundColor: `hsl(${color}, 100%, 50%)`, color: color > 200 && color < 300 ? 'white' : 'black' }}>
+  <nav className="nav" style={{ background: `radial-gradient(circle, hsl(${color}, 100%, 60%), hsl(${color}, 100%, 70%))`, color: color > 200 && color < 300 ? `hsl(${color}, 100%, 90%)` : `hsl(${color}, 100%, 20%)` }}>
     <h2>Theme Changer</h2>
   </nav>
 );
+
 const Skills = ({ color }) => (
-  <div className="skills" style={{ backgroundColor: `hsl(${color}, 100%, 70%)`, color: `hsl(${color}, 100%, 30%)` }}>
-    <FontAwesomeIcon icon={faPython} size="2x" />
-    <FontAwesomeIcon icon={faJsSquare} size="2x" />
-    <FontAwesomeIcon icon={faRust} size="2x" />
-    <FontAwesomeIcon icon={faReact} size="2x" />
-    <FontAwesomeIcon icon={faSwift} size="2x" />
-    <FontAwesomeIcon icon={faJava} size="2x" />
-    <FontAwesomeIcon icon={faLinux} size="2x" />
-    <FontAwesomeIcon icon={faNodeJs} size="2x" />
-    <FontAwesomeIcon icon={faAngular} size="2x" />
-    <FontAwesomeIcon icon={faHtml5} size="2x" />
-    <FontAwesomeIcon icon={faCss3Alt} size="2x" />
+  <div className="skills" style={{ background: `radial-gradient(circle, hsl(${color}, 100%, 60%), hsl(${color}, 100%, 70%))`, color: `hsl(${color}, 100%, 30%)`, hover: `hsl(${color}, 100%, 80%)` }}>
+    <FontAwesomeIcon className="icon" icon={faPython} size="2x" />
+    <FontAwesomeIcon className="icon" icon={faJsSquare} size="2x" />
+    <FontAwesomeIcon className="icon" icon={faRust} size="2x" />
+    <FontAwesomeIcon className="icon" icon={faReact} size="2x" />
+    <FontAwesomeIcon className="icon" icon={faSwift} size="2x" />
+    <FontAwesomeIcon className="icon" icon={faJava} size="2x" />
+    <FontAwesomeIcon className="icon" icon={faLinux} size="2x" />
+    <FontAwesomeIcon className="icon" icon={faNodeJs} size="2x" />
+    <FontAwesomeIcon className="icon" icon={faAngular} size="2x" />
+    <FontAwesomeIcon className="icon" icon={faHtml5} size="2x" />
+    <FontAwesomeIcon className="icon" icon={faCss3Alt} size="2x" />
   </div>
 );
 
-
 const Footer = ({ color }) => (
-  <footer style={{ backgroundColor: `hsl(${color}, 100%, 50%)`, color: color > 200 && color < 300 ? 'white' : 'black', padding: '10px', position: 'fixed', bottom: 0, width: '100%', display: 'flex', justifyContent: 'center' }}>
-    <p style={{ fontFamily: "'Montserrat', sans-serif" }}>© 2023 Theme Changer. All rights reserved.</p>
+  <footer className="footer" style={{ background: `radial-gradient(circle, hsl(${color}, 100%, 60%), hsl(${color}, 100%, 70%))`, color: color > 200 && color < 300 ? `hsl(${color}, 100%, 90%)` : `hsl(${color}, 100%, 20%)`  }}>
+    <p>© 2023 Theme Changer. All rights reserved.</p>
   </footer>
 );
-
-
 
 const App = () => {
   const [hue, setHue] = useState(0);
 
-  // Create a color using the slider value (0-360)
-  const backgroundColor = `hsl(${hue}, 100%, 80%)`;
-  const darkerBackgroundColor = `hsl(${hue}, 100%, 70%)`; // Darker color for components
-
-  // Determine text color based on hue for contrast
-  const textColor = hue > 200 && hue < 300 ? 'black' : 'white';
-
-  // Determine color of the slider's knob
+  const backgroundColor = `radial-gradient(circle, hsl(${hue}, 100%, 70%), hsl(${hue}, 100%, 80%))`;
+  const darkerBackgroundColor = `hsl(${hue}, 100%, 70%)`; 
+  const textColor = hue > 200 && hue < 300 ? `hsl(${hue}, 100%, 90%)` : `hsl(${hue}, 100%, 20%)`;
   const sliderThumbColor = `hsl(${hue}, 100%, 50%)`;
 
   const updateSliderThumbColor = (event) => {
-    // Apply color to thumb
-    event.target.style.setProperty('--thumb-color', sliderThumbColor, 'important');
+    event.target.style.setProperty('--thumb-color', sliderThumbColor,     'important');
     setHue(event.target.value);
   }
 
-     return (
-    <div style={{ backgroundColor, color: textColor, minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '20px' }}>
+  return (
+    <div className="app" style={{ background: backgroundColor, color: textColor, minHeight: '100vh', padding: '20px' }}>
       <Navbar color={hue} />
+
       <div className="sliderContainer">
         <input
           type="range"
@@ -83,24 +75,24 @@ const App = () => {
         />
       </div>
 
-      <h1>Welcome to the Theme Changer Website!</h1>
+      <h1 className="title">Welcome to the Theme Changer Website!</h1>
 
-      <button className="button" style={{ backgroundColor: darkerBackgroundColor, color: textColor }}>
+      <button className="button" style={{ background: `radial-gradient(circle, ${darkerBackgroundColor}, hsl(${hue}, 100%, 60%))`, color: textColor }}>
         Click me
       </button>
 
-      <div className="card" style={{ backgroundColor: darkerBackgroundColor, color: textColor }}>
+      <div className="card" style={{ background: `radial-gradient(circle, ${darkerBackgroundColor}, hsl(${hue}, 100%, 60%))`, color: textColor }}>
         <h2>I'm a card</h2>
         <p>This is some text inside a card.</p>
       </div>
 
-      <h2>My Skills:</h2>
-  <Skills color={hue} />
+      <h2 className="skills-title">My Skills:</h2>
+      <Skills color={hue} />
 
-  <Footer color={hue} />
-</div>
-);
-
+      <Footer color={hue} />
+    </div>
+  );
 };
 
 export default App;
+
